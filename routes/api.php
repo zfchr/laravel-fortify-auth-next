c10c6889a082ad4ab9ba5191e1ccacf678ea29fb
ZidanFachrian <mzidanfachriana@gmail.com>
2023-01-15 16:51:53 +0700

    wip

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/user', [UserController::class, 'authenticated']);
+
+});
